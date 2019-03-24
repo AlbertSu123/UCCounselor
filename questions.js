8253f2c0ab0115db87b0364d7a8f5e722d87fca9
Albert Su <albertsu2001@gmail.com>
2019-03-24 10:10:47 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,70 @@
+
+const questions = [
+  {
+    "question": "What are your idea surroundings for college?",
+    "answer1": "Beach",
+    "answer1Total": "3",
+    "answer2": "Urban/City",
+    "answer2Total": "3",
+    "answer3": "Rural",
+    "answer3Total": "1"
+  },
+  {
+    "question": "What are your ideal Classmates or Friends like?",
+    "answer1": "Funny",
+    "answer1Total": "1",
+    "answer2": "Truthful/Helpful",
+    "answer2Total": "2",
+    "answer3": "Busy",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "What jobs are you looking to do in the future?",
+    "answer1": "Work for UCs",
+    "answer1Total": "2",
+    "answer2": "Business",
+    "answer2Total": "3",
+    "answer3": "Hospitality/Tourism",
+    "answer3Total": "1"
+  },
+  {
+    "question": "What are you hobbies?",
+    "answer1": "Photography",
+    "answer1Total": "1",
+    "answer2": "Parks",
+    "answer2Total": "2",
+    "answer3":
+      "Reading",
+    "answer3Total": "3"
+  },
+  {
+    "question": "What do you want to study in college?",
+    "answer1": "Psychology",
+    "answer1Total": "1",
+    "answer2": "Engineering",
+    "answer2Total": "2",
+    "answer3": "Business",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "What is your ideal city to visit?",
+    "answer1":
+      "Sacramento",
+    "answer1Total": "1",
+    "answer2": "Los Angeles",
+    "answer2Total": "3",
+    "answer3": "San Francisco",
+    "answer3Total": "1"
+  },
+  {
+    "question": "What are your ideal college activities?",
+    "answer1": "Partying",
+    "answer1Total": "1",
+    "answer2": "Volunteering",
+    "answer2Total": "2",
+    "answer3": "Research",
+    "answer3Total": "3"
+  }
+]
